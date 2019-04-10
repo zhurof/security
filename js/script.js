0ea71d12660abf54c793686ebcae54f8ea050121
zhurof <41473707+zhurof@users.noreply.github.com>
2019-04-10 16:45:46 +0200

    Done
    Ждем-с

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ function changeTemplate(){
 	if(screen() != 'phone' && screen() != 'smallphone'){
 		$('.header__menu').css('display','')
 	}
+	catalogFilter(category,page);
 }
 $(document).ready(changeTemplate);
 $(window).resize(changeTemplate);
@@ -25,7 +26,7 @@ $(window).on('load scroll',function(){
 		$('.header').addClass('fixed');
 	}else{
 		$('.header').removeClass('fixed');
-		$('.scroll-btn').removeClass('anim');
+		$('.scroll-btn').removeClass('hidden');
 	}
 })
 $('.menu-btn').click(function(){
@@ -65,7 +66,7 @@ $('input,textarea').on('input change',validateInput);
 
 //Ненужная кнопка скрола
 $('.scroll-btn').click(function(){
-	$(this).addClass('anim');
+	$(this).addClass('hidden');
 	$('html, body').animate({
       scrollTop: +$('.services').offset().top + +$('.services').outerHeight()
     }, 800);
@@ -89,29 +90,6 @@ $('.actions__slider').slick({
 	]
 })
 
-//Слайдер карточек
-function locateSlide(){
-	var index = $(this).index(),
-			amount = +$(this).siblings().length + 1,
-			top = 95/(amount-1)*index,
-			left = 110/(amount-1)*index;
-	$(this).css('transform','translate('+left+'px,'+top+'px)')
-}
-$('.card-slider').each(function(){
-	var slideAmount = $(this).children('.card-slider__item').size();
-	if(slideAmount){
-		$(this).children('.card-slider__item').each(locateSlide)
-	}
-})
-$('.card-slider__item').click(function(){
-	if(!$(this).is(':last-child')){
-		$(this).appendTo($(this).parent());
-		$(this).parent().children().each(locateSlide);
-	}else{
-		$(this).prependTo($(this).parent());
-		$(this).parent().children().each(locateSlide);
-	}
-})
 
 //отзывы
 $('.reviews__persons').slick({
@@ -147,55 +125,16 @@ $('.reviews__slider').slick({
 	]
 })
 // PERSPECTIVE
-/* var wrap = document.querySelector('.card')
-var hover = new perspective.Hover(wrap) */
 var perespectiveOptions = {
-	max: 5,
-  //reverseTilt: true,
+	max: 13,
   scale: 1,
-  perspective: 100
+	reverseTilt: true,
+	reverseTranslate: true,
+  perspective: 600
 }
 $('.card').each(function(){
 	new perspective.Hover(this,perespectiveOptions)
 })
-/* var tiltSettings = [
-{},
-{
-	movement: {
-		imgWrapper : {
-			translation : {x: 10, y: 10, z: 30},
-			rotation : {x: 0, y: -10, z: 0},
-			reverseAnimation : {duration : 200, easing : 'easeOutQuad'}
-		},
-		lines : {
-			translation : {x: 10, y: 10, z: [0,70]},
-			rotation : {x: 0, y: 0, z: -2},
-			reverseAnimation : {duration : 2000, easing : 'easeOutExpo'}
-		},
-		caption : {
-			rotation : {x: 0, y: 0, z: 2},
-			reverseAnimation : {duration : 200, easing : 'easeOutQuad'}
-		},
-		overlay : {
-			translation : {x: 10, y: -10, z: 0},
-			rotation : {x: 0, y: 0, z: 2},
-			reverseAnimation : {duration : 2000, easing : 'easeOutExpo'}
-		},
-		shine : {
-			translation : {x: 100, y: 100, z: 0},
-			reverseAnimation : {duration : 200, easing : 'easeOutQuad'}
-		}
-	}
-}];
-
-function init() {
-	var idx = 0;
-	[].slice.call(document.querySelectorAll('.portfolio__item')).forEach(function(el, pos) { 
-		idx = pos%2 === 0 ? idx+1 : idx;
-		new perspective.Hover(el, tiltSettings[idx-1]);
-	});
-}
-init(); 	 */
 
 //Параллакс
 $('.title-shadow').paroller({ 
@@ -212,9 +151,9 @@ $('.header__menu a[href^="#"]').click(function(e){
 	if($(hash).length){
 		$('.header__menu').css('display','');
 		$('html, body').animate({
-          scrollTop: $(hash).offset().top - $('.header').outerHeight()
-        }, 400);				
-        return false;
+				scrollTop: $(hash).offset().top - $('.header').outerHeight()
+			}, 400);				
+    return false;
 	}
 })
 
@@ -247,29 +186,6 @@ $('.price-block__tabs').on('beforeChange',function(event,slick,currentSlide,next
 	$(this).parents('.price-block').find('.composition img').removeClass('active').eq(index).addClass('active');
 })
 //сетка isotope в портфолио
-var portfolio = {
-	full: 12,
-	laptop: 12,
-	tablet: 9,
-	mobile: 4,
-	smallphone: 4
-};
-var filterClass;
-
-function setPagination(){
-	var amount = $('.portfolio__item').size() || 0,
-			max = portfolio[screen()] || 1;
-	$('.portfolio__pagination li').remove();
-	if(amount > max){
-		$('.portfolio__pagination').css('display','');
-		var pages = Math.ceil(amount/max);
-		for(var i = 0; i<pages;i++){
-			$('.portfolio__pagination').append('<li></li>');
-		}
-	}else{
-		$('.portfolio__pagination').hide();
-	}
-}
 $(document).ready(function(){
 	$('.portfolio__box').css('display','block');
 	$('.portfolio__box').isotope({
@@ -278,14 +194,156 @@ $(document).ready(function(){
 		layoutMode: 'fitRows'
 	});
 })
-$('.portfolio__filter li').click(function(){
-	if(!$(this).is('.active')){
-		filterClass = $(this).data('category');
-	}else{
+
+//Количество карточек на разных разрешениях экрана. Названия ключей соответствуют значениям, возвращаемым функцией screen()
+var portfolio = {
+	full: 12,
+	laptop: 12,
+	tablet: 9,
+	phone: 4,
+	smallphone: 4
+};
+//Глобальные переменные, содержащие информацию о текущих фильтрах
+var category = '*',
+		page = 1;
+
+//Установка постраничной навигации или её сокрытие при ненадобности
+function setPagination(){
+	var amount = $('.portfolio__item').filter(category).size() || 0, //Колчиество карточек, попадающих под фильтр категории
+			max = portfolio[screen()] || 1; //Максимальное количество карточек на странице
+	$('.portfolio__pagination li').remove(); //Удаление старой пагинации
+	if(amount > max){
+		$('.portfolio__nav,.portfolio__arrows').css('display','');
+		var pages = Math.ceil(amount/max); //Количество страниц
+		for(var i = 0; i<pages;i++){
+			$('.portfolio__pagination').append('<li></li>');
+		}
+		$('.portfolio__pagination li').eq(page-1).addClass('active'); //активное состояние для пункта пагинации текущей страницы
 		
+		/*меняем состояние навигационных стрелок и кнопок*/
+		$('.portfolio__arrow,.portfolio__btn').removeClass('disabled');
+		if(page == pages){
+			$('.portfolio__arrow.next,.portfolio__btn.next').addClass('disabled');
+		}
+		if(page == 1){
+			$('.portfolio__arrow.prev,.portfolio__btn.prev').addClass('disabled');
+		}
+	}else{//Если количество карточек на странице меньше максимального, убираем навигацию до лучших времён
+		$('.portfolio__nav,.portfolio__arrows').hide();
 	}
-	$(this).toggleClass('active')
 	
-	$('.portfolio__box').isotope({ filter: '.'+category })
+}
+function catalogFilter(category = '*',page = 1){
+	var itemsPerPage = portfolio[screen()]; //Максимальное количество карточек на странице
+	$('.portfolio__item').removeClass('item'+page); //Сносим классы текущей страницы для старых фильтров
+	$('.portfolio__item').filter(category).slice((page-1)*itemsPerPage,page*itemsPerPage).addClass('item'+page);//Добавляем классы текущей страницы для карточек, сооответствующих новому фильтру
+	$('.portfolio__box').isotope({filter: category + '.item'+page});//Фильтруем
+	setPagination();//Устанавливаем пагинацию (или удаляем при необходимости)
+}
+
+$('.portfolio__filter li').click(function(){
+	$(this).siblings('.active').removeClass('active');
+	if(!$(this).is('.active')){
+		category = '.'+$(this).data('category');		
+	}else{
+		category = '*'
+	}
+	page = 1;
+	$(this).toggleClass('active');	
+	catalogFilter(category,page);
+})
+$('.portfolio__pagination').on('click','li',function(){
+	page = +$(this).index()+1
+	catalogFilter(category,page);
 })
 
+$('.portfolio__btn,.portfolio__arrow').click(function(){
+	var nextPage,
+			maxPages = $('.portfolio__pagination li').length || 0;
+	if($(this).is('.next')){
+		nextPage = +page+1
+	}else{
+		nextPage = +page-1
+	}
+	if(isFinite(nextPage) && nextPage >= 1 && nextPage <= maxPages){
+		page = nextPage;
+		catalogFilter(category,page);
+	}
+})
+
+//слайдер брендов со случайной заменой
+function changeBrand(){
+	var amount = $('.brands__item').size() || 1,
+			visible = $('.brands__item:visible').size() || 1,
+			index = Math.round(Math.random()* (visible - 1)), //номер случайного элемента среди видимых
+			commonIndex = $('.brands__item:visible').eq(index).index(), //номер случайного элемента среди всех
+			newIndex = Math.round(Math.random()*(amount - visible) + visible); //выбираем случайный индекс из невидимых элементов		
+
+	$('.brands__item').eq(index).clone().appendTo('.brands__box');
+	$('.brands__item').eq(index).replaceWith($('.brands__item').eq(newIndex));		
+}
+$(document).ready(function(){
+	setInterval(changeBrand,3000);
+})
+//Самописный "плагин" слайдера-стопки
+$.fn.stackSlider = function(options){
+	var defaultSettings = {
+		offsetX: 110,
+		offsetY: 95,
+		responsive: false
+	}
+	var settings = $.extend(defaultSettings,options);
+	var slider = this, 
+			amount = slider.children().size(),
+			responsive = settings.responsive;
+	function init(){
+		if(responsive){
+			for(key in responsive){
+				if($(window).width()<key){
+					settings = $.extend(settings,responsive[key]);
+					break;
+				}
+			}
+		}
+		locate();
+	}
+	function locate(){
+		slider.children().each(function(){
+			var index = $(this).index(),
+					top = settings.offsetY/(amount-1)*index,
+					left = settings.offsetX/(amount-1)*index;
+			$(this).css('transform','translate('+left+'px,'+top+'px)')
+		})
+	}	
+	init();
+	$(window).resize(init);
+	
+	slider.children().click(function(){
+		if(!$(this).is(':last-child')){
+			$(this).appendTo(slider);
+		}else{
+			$(this).prependTo(slider);
+		}
+		locate();
+	})
+}
+$('.card-slider').stackSlider({
+	responsive: {
+		1820: {
+			offsetX: 40,
+			offsetY: 47
+		},
+		1280: {
+			offsetX: 34,
+			offsetY: 38
+		},
+		1024: {
+			offsetX: 257,
+			offsetY: 122
+		},
+		768: {
+			offsetX: 0,
+			offsetY: 70
+		}
+	}
+});
